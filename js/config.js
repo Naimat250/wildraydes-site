a07ceb8a0e59c5ef4510f273ff6f2adb429ac47a
Muhammad Naimat Ullah <65623633+Naimat250@users.noreply.github.com>
2024-12-18 10:53:26 +0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
         userPoolId: 'us-west-1_cJy2c7D1Z', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2sig2dg2v3tobouqt65sgp40kf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '27qos8olja983mb58agran3f52' // e.g. us-east-2
+        userPoolClientId: '27qos8olja983mb58agran3f52', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
