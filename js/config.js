ccb374407f0bc59218ce62b86e5b81450aa62461
Muhammad Naimat Ullah <65623633+Naimat250@users.noreply.github.com>
2024-12-18 11:17:45 +0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,21 +1,12 @@
-import { UserManager } from 'oidc-client-ts';
-
-const cognitoAuthConfig = {
-    authority: "https://cognito-idp.us-west-1.amazonaws.com/us-west-1_cJy2c7D1Z",
-    client_id: "27qos8olja983mb58agran3f52",
-    redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",
-    response_type: "code",
-    scope: "email openid phone"
-};
-
-// create a UserManager instance
-export const userManager = new UserManager({
-    ...cognitoAuthConfig,
-});
-
-export async function signOutRedirect () {
-    const clientId = "27qos8olja983mb58agran3f52";
-    const logoutUri = "<logout uri>";
-    const cognitoDomain = "https://us-west-1cjy2c7d1z.auth.us-west-1.amazoncognito.com";
-    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-1_cJy2c7D1Z', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '27qos8olja983mb58agran3f52', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
+  
+        
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
 };
