d5596585fdc51e6cdd2acb9a7de183c8a8126e1f
Muhammad Naimat Ullah <65623633+Naimat250@users.noreply.github.com>
2024-12-18 11:42:57 +0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-1_cJy2c7D1Z', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '27qos8olja983mb58agran3f52', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-1_nTdElgMk0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '542k0eaet7odjau1cq3l2576jf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-1' // e.g. us-east-2
   
         
